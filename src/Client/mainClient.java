d52649d85dd51cd00d7184a85305e4aa67af8fdf
Guaiter12 <mv120707@gmail.com>
2026-02-23 13:01:40 +0100

    terzo commit

--- src/Client/mainClient.java
+++ src/Client/mainClient.java
@@ -1,23 +1,31 @@
 package Client;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class mainClient {
     public static void main(String[] args) {
-        System.out.print("Client: avvio client");
+        System.out.println("Client: avvio client");
 
         try {
             Socket socket = new Socket("localhost", 3000);
+            System.out.println("Client: connesso al server!");
+
             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-            pw.println("Ciao!");
-            System.out.println("CLIENT: messaggio inviato");
-
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String risposta = br.readLine();
-            System.out.println("CLIENT: il server ha risposto -> " + risposta);
 
+
+            String messaggio = "Ciao server, come stai?";
+            pw.println(messaggio);
+            System.out.println("CLIENT -> SERVER: " + messaggio);
+
+
+            String risposta = br.readLine();
+            System.out.println("SERVER -> CLIENT: " + risposta);
 
             pw.close();
             br.close();
--- src/Server/mainServer.java
+++ src/Server/mainServer.java
@@ -1,6 +1,9 @@
 package Server;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,33 +11,32 @@ public class mainServer {
     public static void main(String[] args) {
         System.out.println("Sono il server");
 
-        ServerSocket serverSocket;
-
         try {
-            serverSocket = new ServerSocket(3000);
+            ServerSocket serverSocket = new ServerSocket(3000);
             System.out.println("Server: in attesa di richieste");
 
-
             Socket socket = serverSocket.accept();
+            System.out.println("Server: client connesso!");
 
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String messaggio = br.readLine();
-            System.out.println("SERVER: il client ha scritto -> " + messaggio);
-
             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-            pw.println("Messaggio ricevuto!");
 
+            // Legge il messaggio dal client
+            String messaggio = br.readLine();
+            System.out.println("CLIENT -> SERVER: " + messaggio);
+
+            // Risposta preimpostata del server
+            String risposta = "Ciao client, messaggio ricevuto!";
+            pw.println(risposta);
+            System.out.println("SERVER -> CLIENT: " + risposta);
 
             br.close();
             pw.close();
             socket.close();
             serverSocket.close();
 
-
         } catch (IOException e) {
-            System.err.println("Could not listen on port: 3000");
-            e.printStackTrace();
+            System.err.println("Errore: " + e.getMessage());
         }
     }
 }
-
