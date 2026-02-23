00b51a692b0d2cf813b182ce3a3b17a60f752383
Guaiter12 <mv120707@gmail.com>
2026-02-23 12:14:09 +0100

    pirmo commit

--- src/Client/mainClient.java
+++ src/Client/mainClient.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+package Client;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class mainClient {
+public static void main(String[] args) {
+System.out.print("Client: avvio client");
+ try{
+     Socket socket = new Socket ("local host",3000);
+     OutputStream outputStream = socket.getOutputStream();
+     PrintWriter pw = new PrintWriter(outputStream);
+     pw.println("Ciao!");
+     pw.flush();
+     System.out.println("CLIENT:il client ha inviato un messaggio");
+
+ } catch (UnknownHostException e) {
+     System.err.println("Errore nella connessione al server");
+ } catch (IOException e) {
+     throw new RuntimeException(e);
+ }
+}
+
+
+
 }
--- src/Server/mainServer.java
+++ src/Server/mainServer.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+package Server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class mainServer {
+    public static void main(String[] args) {
+
+        System.out.println("Sono il server");
+    }
+
+    ServerSocket serverSocket;
+
+    {
+        try {
+            serverSocket = new ServerSocket(3000);
+            System.out.println("Server: in attesa di richieste");
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 3000");
+
+            InputStream inputStream = Socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+                String messsaggio = br.readLine();
+
+
+            System.out.println("SERVER: il client" + Socket + "ha scrittto il messaggio" + messaggio);
+
+        }
+    }
 }
+
